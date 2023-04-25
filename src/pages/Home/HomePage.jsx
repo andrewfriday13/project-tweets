373ba537e13776b84ae7eb10dacd0e41c4733e1d
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2023-04-25 22:27:56 +0200

    add scale

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,11 +1,9 @@
-
+import css from './HomePage.module.css'
 
 export const HomePage = () => {
 
     return(
-        <>
-        <h1>Home Page</h1>
-        </>
+        <h1 className={css.homePage} >Home Page</h1>
     )
 }
 
